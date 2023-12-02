080bef22bc799f5daa851b9f5a898072c66df330
igflores <139146133+igflores@users.noreply.github.com>
2023-12-02 08:14:19 -0800

    Updated the archives: cypress.config.js, package-lock.json, package.json and added jsconfig.json

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-## **Personal repository for Automated testing practices**
+# 🧪**Testing Automation - Cypress 13 | Isabel Gonzalez's personal repository for testing practices**
+
+# Project structure
+```
+    PONER LA ESTRUCTURA AQUI <<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>
+```
+
+
+# How to get started:
+>**1. Clone the Project:**
+    ```
+    git clone git@github.com:igflores/QAIsabel-Cypress.git
+    ```
+
+>**2. Install all dependencies:**
+    ```
+    npm ci
+    ```
+
+*The "ci" command is for installing all locked project dependencies
+
+>**3. To open the Cypress app, run the command:**
+    ```
+    npm test
+    ```
+
+>**4. To run tests and generate XML and HTML reports, execute:**
+    ```
+    npm run file */**/<filename>
+    ```
+Where the variable "file" is:
+```
+`cypress run --browser chrome --reporter cypress-multi-reporters --reporter-options configFile=jsconfig.json --record --key {key} --spec`
+```
+Which is a shortcut to run tests from a specified directory using the Chrome browser, generating an XML report for Jira import, an HTML report for display, and additionally updating the Cypress Project Dashboard.
--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,25 @@
-const { defineConfig } = require("cypress");
+// const { defineConfig } = require("cypress");     // Common JS exportation
+import { defineConfig } from 'cypress';   //ES MODULE: system modulation structure. It is used to import and exportx
 
-module.exports = defineConfig({
-  e2e: {
+export default defineConfig({
+  viewportHeight: 1080,         // Default height in pixels for the application under tests' viewport. 
+  viewportWidth: 1920,          // Default width in pixels for the application under tests' viewport.
+  watchForFileChanges: false,   // Disables automatic file change monitoring, allowing manual control over when tests are rerun after making modifications.
+  chromeWebSecurity: false,     // Disables the Chrome browser's web security policy, allowing bypassing CORS restrictions during test execution.
+  video: false,                 // No video recording of the test execution will be performed
+  retries: process.env.CI ? 2 : 0,  // Number of times to retry a failed test: 2 times on CI and 0 for local configuration
+  // testIsolation: false,         // Test execution will not occur in isolation. My test cases will be independent, as a good practice
+  
+  // --------------- E2E Testing runner ------------------------------------------------------
+  e2e: {  
+    specPattern: ['cypress/tests/**/*.cy.{js,jsx,ts,tsx}'],     // Glob pattern to determine what test files to load
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env: {
+      baseUrlDemoQA: 'https://xxx',
+      baseUrlSawglab: 'https://xxx',
+    }
   },
 });
