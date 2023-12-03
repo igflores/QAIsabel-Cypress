ab520e7a3742e17be686b411977f19a833bfc26e
igflores <139146133+igflores@users.noreply.github.com>
2023-12-03 08:56:41 -0800

    Test & Doc(DemoQA SUT): I installed the 'Faker' library. Faker for Cypress is a JavaScript library that generates realistic fake data for testing purposes, facilitating the simulation of various scenarios within Cypress tests. Additionally, the 'practiceForm.cy.js' test for the System Under Test (SUT): DemoQA (https://demoqa.com/automation-practice-form) was added to practice different form elements. Its corresponding Page Object Model 'practiceForm' was created. POM, or Page Object Model, is a design methodology applied in Cypress automated testing, which structures and abstracts the interaction with web pages. This enhances code maintainability and clarity by representing each page as an object. The corresponding documentation was added for the 'practiceForm.cy.js' test, which includes: description, acceptance criteria, scope, Out of Scope (OOS), and the business rules.

---
+++ cypress/tests/DemoQA/practiceForm.cy.js
@@ -0,0 +1,72 @@
+import { removeLogs } from '../../support/utils/RemoveLogs';
+import { practiceForm } from '../../support/pages/DemoQA/practiceForm';
+
+describe('GX-34872 | ✅ToolsQA | Forms | Practice Form', () => {
+	beforeEach('Precondition: to use the Practice Form', () => {
+		cy.visit('https://demoqa.com/automation-practice-form');
+		cy.url().should('contain', 'practice-form');
+	});
+
+	it('GX-34871 | TC01: Validate that a popup displays all the valid data', () => {
+		const formData = practiceForm.fillAndGetInputData();
+		const check = [
+			{ data: 'firstNameInput', value: formData.firstName },
+			{ data: 'lastNameInput', value: formData.lastName },
+			{ data: 'emailInput', value: formData.email },
+			{ data: 'mobileInput', value: formData.mobile },
+			{ data: 'currentAddressInput', value: formData.address },
+		];
+		check.forEach(({ data, value }) => {
+			practiceForm.get[data]().should('have.value', value);
+		});
+
+		practiceForm.selectGender();
+		practiceForm.get.gender().then(select => {
+			expect(select.text()).to.contain(Cypress.env('genderSelected'));
+		});
+
+		practiceForm.SelectHobbies();
+		practiceForm.get.hobbies().then(selectHobbie => {
+			expect(selectHobbie.text()).to.contain(Cypress.env('hobbiesSelected'));
+		});
+
+		practiceForm.uploadPicture();
+		practiceForm.get.pictureButton().should('contain.value', 'upexlogo.png');
+
+		practiceForm
+			.fillAndSelectSubject()
+			.invoke('text')
+			.then(subjectName => {
+				cy.log(subjectName);
+				practiceForm.get.subjectsContainer().should('contain.text', subjectName);
+			});
+
+		practiceForm.selectBirthDay();
+		practiceForm.get.BirthdayInputCompleted().should('not.have.value', '');
+		const dateOfBirthField = practiceForm.get.BirthdayInputCompleted();
+		dateOfBirthField.invoke('val').then(dateOfBirth => {
+			expect(dateOfBirth).to.match(/^(0[1-9]|[12]\d|3[01]) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (19\d\d|20\d\d|2100)$/);
+		});
+
+		practiceForm
+			.selectState()
+			.invoke('text')
+			.then(state => {
+				cy.log(state);
+				practiceForm.get.state().should('be.visible');
+			});
+
+		practiceForm
+			.selectCity()
+			.invoke('text')
+			.then(city => {
+				cy.log(city);
+				practiceForm.get.city().should('be.visible');
+			});
+
+		practiceForm.submitForm();
+		practiceForm.get.popup().should('contain.text', 'Thanks for submitting the form');
+	});
+});
+
+removeLogs();
